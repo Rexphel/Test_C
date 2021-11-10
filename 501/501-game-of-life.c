9c511b08a5d5a46a9adf5e633583ba38e593be0b
Rexphel <56548269+Rexphel@users.noreply.github.com>
2021-11-10 11:57:47 +0100

    fix if again

--- 501/501-game-of-life.c
+++ 501/501-game-of-life.c
@@ -163,7 +163,7 @@ void evolution_step()
 
          for (int k=-1; k<=1; k++){
             for (int l=-1; l <= 1; l++){
-               if (!(i+k < 0) && !(i+k > FieldHeight) && !(j+l < 0) && !(j+l > FieldWidth)){
+               if (!(i+k < 0) || !(i+k > FieldHeight) || !(j+l < 0) || !(j+l > FieldWidth)){
                   NachbarnLebend += cells[i+k][j+l];
                }
             }
